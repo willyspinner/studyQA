c2d870dce743c30efc28542f2fa8011c09384fcb
willyspinner <wilsonjusuf1998@gmail.com>
2021-03-02 18:18:04 -0800

    first commit

---
+++ README.md
@@ -0,0 +1,12 @@
+
+Things to do:
+- [x] Create basic search engine using whoosh
+- [x] create markdown file ingestion.
+- [ ] use basic transformers extractive QA pipeline
+- [ ] (search engine) create microsoft word ingestion.
+- [ ] (search engine) create pdf ingestion.
+
+Ideas to implement:
+- [TQA dataset](https://allenai.org/data/tqa), for textbook question answering
+- [TQA model paper](https://www.aclweb.org/anthology/P19-1347.pdf) to tackle the above. Multimodal understanding 
+
---
+++ search.py
@@ -0,0 +1,79 @@
+from whoosh.index import create_in, open_dir
+from whoosh.qparser import QueryParser
+from whoosh.fields import TEXT, Schema, ID
+from os import path, mkdir, walk
+from pathlib import Path
+
+
+schema = Schema(title=TEXT(stored=True), filepath=ID(stored=True), content=TEXT)
+
+if not path.exists("./index"):
+    mkdir("./index")
+    index = create_in("./index", schema)
+else:
+    index = open_dir('./index')
+
+queryparser = QueryParser("content", index.schema)
+
+def index_files(dirpath):
+    """
+    traverses the filetree from dirpath and index all indexable
+    files (so far, markdown)
+
+    """
+    global index
+    writer = index.writer()
+    abspath = str(Path(dirpath).expanduser())
+    for rootdir, dirs, files in walk(abspath):
+        print("indexing files in ", rootdir)
+        for file in files:
+            filepath = path.join(rootdir, file)
+            index_file(filepath, writer, do_commit=False)
+    writer.commit()
+
+
+def search(querystring):
+    """
+    searches the 'content' for the query string
+
+    """
+    global index, queryparser
+    query = queryparser.parse(querystring)
+    with index.searcher() as searcher:
+        results = searcher.search(query)
+        for result in results:
+            print(result)
+
+
+def index_file(filepath, ix_writer=None, do_commit=True):
+    """
+    main function to index an individual file. if do_commit is True,
+    then it will commit the writer
+
+    """
+    global index
+    if ix_writer is None:
+        ix_writer = index.writer()
+
+    if filepath.endswith('.md'):
+        index_markdown(filepath, ix_writer)
+    else:
+        pass
+    if do_commit:
+        ix_writer.commit()
+
+
+def index_markdown(markdown_filepath, ix_writer):
+    file = path.basename(markdown_filepath)
+    with open(markdown_filepath) as f:
+        content = f.read()
+    # Do any preprocessing here, but the QA model may also read from the filepath.
+    ix_writer.add_document(title=file, content=content, filepath=markdown_filepath)
+
+
+
+
+
+
+
+
